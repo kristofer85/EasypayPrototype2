06ac8375de516513a3672b2015ede24311d907cc
Erla Hardardottir <erlah@mac.com>
2015-05-06 16:30:03 +0000

    created AddPruduct class and made ProductList java class pretteeeeeeeh

---
+++ app/src/main/java/com/example/notandi/easypayprototype/AddProduct.java
@@ -0,0 +1,8 @@
+package com.example.notandi.easypayprototype;
+
+/**
+ * Created by erla on 06/05/15.
+ */
+public class AddProduct {
+
+}
--- app/src/main/java/com/example/notandi/easypayprototype/ProductList.java
+++ app/src/main/java/com/example/notandi/easypayprototype/ProductList.java
@@ -1,7 +1,11 @@
 package com.example.notandi.easypayprototype;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 /**
  * Created by erla on 06/05/15.
@@ -12,6 +16,18 @@ public class ProductList extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_list);
         final String user = getIntent().getStringExtra("User_Name");
+        final String Amount = getIntent().getStringExtra("Amount");
+        ImageButton btnPlus = (ImageButton) findViewById(R.id.btnAdd);
+        TextView lblUser = (TextView) findViewById(R.id.userProduct);
+        TextView lblAmount = (TextView) findViewById(R.id.lblAmount2);
+        lblUser.setText(user);
+        lblAmount.setText(Amount + " ISK");
 
+        btnPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent (view.getContext(), ProductList.class);
+            }
+        });
     }
 }
